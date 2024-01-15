1c3696c6a393d26c17e8ca119a936c18312f7795
Maëlle <maelle.bitsindou2@etu.univ-lorraine.fr>
2024-01-15 10:14:09 +0100

    2eme push

--- mbash.c
+++ mbash.c
@@ -1,23 +1,32 @@
+// Authors: Célie Ponroy et Maëlle Bitsinodu
+
+//insertion des bibliothèques
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
-#define MAXLI 2048
+
+//Constantes
+#define MAXLI 2048 //taille maximale de la ligne de commande
+//#define MAXARG : taille maximale du nombres d'arguments
+
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
 void mbash();
-int main(int argc, char** argv) {
-  while (1) {
+
+//fonction qui permet de lire le fichier .mbashrc
+int main(int argc, char** argv) { // argc: nb d'arguments, argv: tableau de chaine de caractères
+  while (1) { 
     printf("Commande: ");
-    fgets(cmd, MAXLI, stdin);
+    fgets(cmd, MAXLI, stdin); 
     mbash(cmd);
   }
   return 0;
 }
 
-void mbash() {
+void mbash() { // fonction qui permet d'executer une commande
   printf("Execute: %s", cmd);
   system(cmd);
 }
